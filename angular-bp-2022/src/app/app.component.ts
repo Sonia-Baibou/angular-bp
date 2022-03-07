ca512133b2a968cebe818ebe67b507cbabace2b8
Sonia-Baibou <s.baibou@ohmycode.io>
2022-03-07 14:52:14 +0100

    etape 1 - modules

---
+++ angular-bp-2022/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+// décorateur - metadatas
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  title = 'angular-bp-2022';
+
+  prop = true; 
+
+
+
+}// ne rien écrire après cette ligne
